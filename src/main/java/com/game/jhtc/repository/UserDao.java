7a43ce1133fb6304308cd4a78f3122c4da244ecb
SnookerY <snookery@outlook.com>
2017-10-18 19:06:33 +0800

    缓存配置添加

--- src/main/java/com/game/jhtc/repository/UserDao.java
+++ src/main/java/com/game/jhtc/repository/UserDao.java
@@ -19,32 +19,32 @@ public interface UserDao {
 	 * @param gid
 	 * @return
 	 */
-	public User findByGid(int gid);
+	User findByGid(int gid);
 	
 	/**
 	 * 根据uid查询用户信息
 	 * @param uid
 	 * @return
 	 */
-	public User findByUid(String uid);
+	User findByUid(String uid);
 	
 	/**
 	 * 根据客户端传来的uid自动生成gid，
 	 * 其余数据默认生成，再由客户端通过修改接口上报玩家数据
 	 * @param users
 	 */
-	public void createGid(User user);
+	void createGid(User user);
 	
 	/**
 	 * 根据gid修改玩家rank数据信息
 	 * @param user
 	 */
-	public void update(User user);
+	void update(User user);
 	
 	/**
 	 * 查询用户全部信息（前50名）
 	 * @return
 	 */
-	public List<User> findAll();
+	List<User> findAll();
 	
 }
